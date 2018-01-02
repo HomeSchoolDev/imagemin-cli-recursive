ad5724fe78dbddf702f3e4a8612f53fdf10bfe3d
Derek Maurer <derek@mocaapps.com>
2018-01-02 17:08:02 -0600

    first

---
+++ cli.js
@@ -0,0 +1,136 @@
+#!/usr/bin/env node
+'use strict';
+const fs = require('fs');
+const path = require('path');
+const arrify = require('arrify');
+const meow = require('meow');
+const getStdin = require('get-stdin');
+const imagemin = require('imagemin');
+const ora = require('ora');
+const plur = require('plur');
+const stripIndent = require('strip-indent');
+
+const cli = meow(`
+	Usage
+	  $ imagemin <path|glob> ... --out-dir=build [--plugin=<name> ...]
+	  $ imagemin <file> > <output>
+	  $ cat <file> | imagemin > <output>
+
+	Options
+	  -p, --plugin     Override the default plugins
+	  -o, --out-dir    Output directory
+	  -r, --recursive  Run the command recursively
+
+	Examples
+	  $ imagemin images/* --out-dir=build
+	  $ imagemin images --recursive --out-dir=build
+	  $ imagemin foo.png > foo-optimized.png
+	  $ cat foo.png | imagemin > foo-optimized.png
+	  $ imagemin --plugin=pngquant foo.png > foo-optimized.png
+`, {
+	string: [
+		'plugin',
+		'out-dir'
+	],
+	boolean: [
+		'recursive'
+	],
+	alias: {
+		p: 'plugin',
+		o: 'out-dir',
+		r: 'recursive'
+	}
+});
+
+const DEFAULT_PLUGINS = [
+	'gifsicle',
+	'jpegtran',
+	'optipng',
+	'svgo'
+];
+
+const requirePlugins = plugins => plugins.map(x => {
+	try {
+		return require(`imagemin-${x}`)(); // eslint-disable-line import/no-dynamic-require
+	} catch (err) {
+		console.error(stripIndent(`
+			Unknown plugin: ${x}
+
+			Did you forgot to install the plugin?
+			You can install it with:
+
+			  $ npm install -g imagemin-${x}
+		`).trim());
+		process.exit(1);
+	}
+});
+
+const isDir = path => fs.lstatSync(path).isDirectory();
+
+const run = (input, opts) => {
+	if (opts.recursive) {
+		const filtered = input.filter(input => isDir(input));
+		filtered.forEach(dir => {
+			// recursively run for each directory found
+			const children = fs.readdirSync(dir).map(f => path.join(dir, f));
+			run(children, Object.assign({}, opts, {
+				outDir: path.join(opts.outDir, path.parse(dir).name)
+			}));
+		});
+
+		if (filtered.length === input.length) {
+			// all inputs were folders
+			return;
+		}
+	}
+
+	opts = Object.assign({plugin: DEFAULT_PLUGINS}, opts);
+
+	const use = requirePlugins(arrify(opts.plugin));
+	const spinner = ora('Minifying images');
+
+	if (Buffer.isBuffer(input)) {
+		imagemin.buffer(input, {use}).then(buf => process.stdout.write(buf));
+		return;
+	}
+
+	if (opts.outDir) {
+		spinner.start();
+	}
+
+	imagemin(input, opts.outDir, {use})
+		.then(files => {
+			if (!opts.outDir && files.length === 0) {
+				return;
+			}
+
+			if (!opts.outDir && files.length > 1) {
+				console.error('Cannot write multiple files to stdout, specify a `--out-dir`');
+				process.exit(1);
+			}
+
+			if (!opts.outDir) {
+				process.stdout.write(files[0].data);
+				return;
+			}
+
+			spinner.stop();
+
+			console.log(`${files.length} ${plur('image', files.length)} minified`);
+		})
+		.catch(err => {
+			spinner.stop();
+			throw err;
+		});
+};
+
+if (cli.input.length === 0 && process.stdin.isTTY) {
+	console.error('Specify at least one filename');
+	process.exit(1);
+}
+
+if (cli.input.length > 0) {
+	run(cli.input, cli.flags);
+} else {
+	getStdin.buffer().then(buf => run(buf, cli.flags));
+}
---
+++ test.js
@@ -0,0 +1,58 @@
+import fs from 'fs';
+import execa from 'execa';
+import pify from 'pify';
+import test from 'ava';
+
+process.chdir(__dirname);
+
+const fsP = pify(fs);
+
+test('show help screen', async t => {
+	t.regex(await execa.stdout('./cli.js', ['--help']), /Minify images/);
+});
+
+test('show version', async t => {
+	t.is(await execa.stdout('./cli.js', ['--version']), require('./package.json').version);
+});
+
+test('optimize a GIF', async t => {
+	const buf = await fsP.readFile('fixtures/test.gif');
+	t.true((await execa.stdout('./cli.js', {input: buf})).length < buf.length);
+});
+
+test('optimize a JPG', async t => {
+	const buf = await fsP.readFile('fixtures/test.jpg');
+	t.true((await execa.stdout('./cli.js', {input: buf})).length < buf.length);
+});
+
+test('optimize a PNG', async t => {
+	const buf = await fsP.readFile('fixtures/test.png');
+	t.true((await execa.stdout('./cli.js', {input: buf})).length < buf.length);
+});
+
+test('optimize a SVG', async t => {
+	const buf = await fsP.readFile('fixtures/test.svg');
+	t.true((await execa.stdout('./cli.js', {input: buf})).length < buf.length);
+});
+
+test('output error on corrupt images', async t => {
+	t.throws(execa('./cli.js', ['fixtures/test-corrupt.jpg']));
+});
+
+test('support plugins', async t => {
+	const buf = await fsP.readFile('fixtures/test.png');
+	const data = await execa.stdout('./cli.js', ['--plugin=pngquant'], {input: buf});
+	const compareData = await execa.stdout('./cli.js', {input: buf});
+	t.true(data.length < compareData.length);
+});
+
+test('error when trying to write multiple files to stdout', async t => {
+	const err = await t.throws(execa('./cli.js', ['fixtures/test.{jpg,png}']));
+	t.is(err.stderr.trim(), 'Cannot write multiple files to stdout, specify a `--out-dir`');
+});
+
+test('throw on missing plugins', async t => {
+	const buf = await fsP.readFile('fixtures/test.png');
+	const err = await t.throws(execa('./cli.js', ['--plugin=unicorn'], {input: buf}));
+	t.regex(err.stderr, /Unknown plugin: unicorn/);
+});
